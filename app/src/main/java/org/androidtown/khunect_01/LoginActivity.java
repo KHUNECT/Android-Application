dbd480e802760120a51890d3483ce8d020455115
kangho-Noh <rkdgh98@khu.ac.kr>
2018-10-29 00:56:42 +0900

    Loading ~ Login activity

--- app/src/main/java/org/androidtown/khunect_01/LoginActivity.java
+++ app/src/main/java/org/androidtown/khunect_01/LoginActivity.java
@@ -2,12 +2,31 @@ package org.androidtown.khunect_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long   backPressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime)
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            backPressedTime = tempTime;
+            Toast.makeText(getApplicationContext(), "종료하려면 한 번 더 누르세요..", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/org/androidtown/khunect_01/MainActivity.java
+++ app/src/main/java/org/androidtown/khunect_01/MainActivity.java
@@ -11,10 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //자동로그인이 아니면
+        startLoginActivity();
+
+        //로딩화면
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 
+
+    public void startLoginActivity(){
+        Intent intent2 = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent2);
+    }
+
+
+    //필없는거. 나중에 지울 것임
     public void onClickedLogin(View view) {
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
     }
+
 }
