bdb82ce3df4c948df85689d27c4cbdffa250f266
kangho-Noh <rkdgh98@khu.ac.kr>
2018-10-28 21:17:50 +0900

    first

---
+++ app/src/main/java/org/androidtown/khunect_01/MainActivity.java
@@ -0,0 +1,20 @@
+package org.androidtown.khunect_01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickedLogin(View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+}
